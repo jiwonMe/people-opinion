313a4981a46c7c8858c29f4244a23bacfbaa87c6
jiwonMe <park@jiwon.me>
2024-12-21 12:28:12 +0900

    opinion api to dynamic

--- app/api/opinions/route.ts
+++ app/api/opinions/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { google } from 'googleapis';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   try {
     const auth = new google.auth.GoogleAuth({
