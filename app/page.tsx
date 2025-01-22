891a3a1f8fcc91a7b8688f47f69850731623af5a
miridih-jwpark02 <jwpark02@miridih.com>
2025-01-22 18:07:39 +0900

    Add link to review card in Home component: Users can now navigate to the 'Finish' page to review their card.

--- app/page.tsx
+++ app/page.tsx
@@ -86,6 +86,9 @@ export default function Home() {
           지금 바로 의견 보내기
           </CTAButton>
         </Link>
+        <Link href="/finish" className='font-bold text-sm text-gray-500 underline fixed bottom-[calc(10vh-32px)] left-1/2 -translate-x-1/2'>
+            내 카드 다시 확인하기
+        </Link>
         <div className='text-white text-xs text-center bg-black/50 rounded-tl-xl px-4 py-2'>
           <Link 
             href="https://www.instagram.com/valid.kr/" 
